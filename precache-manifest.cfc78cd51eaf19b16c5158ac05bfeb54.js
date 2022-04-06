7f5cbc71ac90f5c6debebfb8b9b34d6180801800
minhchien635 <minhchien0407.vk.1718@gmail.com>
2022-04-06 15:24:23 +0700

    Updates

---
+++ precache-manifest.cfc78cd51eaf19b16c5158ac05bfeb54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "0abe8fe4633b8ea06557",
+    "url": "/sleep-cycle/static/css/main.24fd18d4.chunk.css"
+  },
+  {
+    "revision": "0abe8fe4633b8ea06557",
+    "url": "/sleep-cycle/static/js/main.0abe8fe4.chunk.js"
+  },
+  {
+    "revision": "8af76b204250c9c9d5fd",
+    "url": "/sleep-cycle/static/js/1.8af76b20.chunk.js"
+  },
+  {
+    "revision": "bc4cf62524a9b4add039",
+    "url": "/sleep-cycle/static/js/runtime~main.bc4cf625.js"
+  },
+  {
+    "revision": "56d6e35d5e97cda8517ddc947b70ae52",
+    "url": "/sleep-cycle/index.html"
+  }
+];
